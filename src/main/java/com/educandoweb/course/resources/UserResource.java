3cbe0acec3717268eb9a2d4d5132e61168d9564d
Gabriel Pedrico <gabriel.pedrico@fatec.sp.gov.br>
2021-02-05 13:23:12 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,26 +1,39 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController // Para indicar que essa classe é um recursoweb que é implementado por um
 				// controlador rest.
 @RequestMapping(value = "/users") // Para indicar o nome do recurso.
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 	// Metodo que sera um end point para acessar os usuarios, o ResponseEntity é um
 	// tipo especifico do spring para retornar respostas de requisicao web, o
 	// findAll é o metodo que estou criando abaixo para retornar os usuarios.
 	@GetMapping //Pra indicar que esse metodo sera um metodo que responde a requisicao do tipo get do http
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "12345"); // Instanciando Usuario.
-		return ResponseEntity.ok().body(u); // ResponseEntity.ok() para retornar a resposta com sucesso e o .body(u)
-											// para retornar o corpo da resposta esse meu usuario u que acabei de
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();// Instanciando Usuario.
+		return ResponseEntity.ok().body(list); // ResponseEntity.ok() para retornar a resposta com sucesso e o .body(list)
+											// para retornar o corpo da resposta esse meus usuarios da list que acabei de
 											// instanciar
-
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
